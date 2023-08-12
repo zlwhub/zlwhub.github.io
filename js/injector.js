bc376aec4abefbc1f0ac16fad48a742d9fee6dd2
zlwhub <iszlw@foxmail.com>
2023-08-12 17:31:00 +0800

    Site updated: 2023-08-12 17:31:00

---
+++ js/injector.js
@@ -0,0 +1,1 @@
+const{root:siteRoot="/themes/fluid/source/"}=hexo.config;hexo.extend.injector.register("body_begin",'<div id="web_bg"></div>'),hexo.extend.injector.register("body_end",`<script src="${siteRoot}js/backgroundize.js"><\/script>\n  <link defer rel="stylesheet" href="${siteRoot}css/backgroundize.css" />\n  `);
